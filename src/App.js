99d0e60a9eb3ad893a6158215354ea14accbbcfa
Chstphr <somto565@gmail.com>
2023-03-02 15:32:57 +0100

    writing up he jsx

--- src/App.js
+++ src/App.js
@@ -6,9 +6,32 @@ function App() {
   const url =`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=7b47420e7952bbe306d64d5fdd598584`
 
   return (
-    <>
-    
-    </>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60`F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65'F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            12 MPH
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
