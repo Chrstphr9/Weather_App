a92aa708120547095aa856cc770fcd586d490cb4
Chstphr <somto565@gmail.com>
2023-03-02 08:53:23 +0100

    added the api address

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import axios from "axios";
 
 function App() {
+
+  const url =`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=7b47420e7952bbe306d64d5fdd598584`
+
   return (
     <>
-    hello
+    
     </>
   );
 }
